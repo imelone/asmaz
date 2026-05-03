cff88a0a41f682ae55b0dfc11f2de620bf85fe7c
ignacio melone <ignacio.melone@gmail.com>
2026-05-03 20:47:29 -0300

    style fixes

--- src/components/ui.ts
+++ src/components/ui.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background-color: white;
+  color: #1f2937;
   padding: 1.5rem;
   border-radius: 0.5rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
